f29650d5472867e8f8717a89930662754d38e3eb
Gabriel Casals <83978645+casalsgh@users.noreply.github.com>
2023-06-22 15:34:30 -0300

    Update purchase.go

--- data/purchase.go
+++ data/purchase.go
@@ -8,33 +8,33 @@ import (
 
 // Purchase defines a set of keys for a purchase event.
 type Purchase struct {
-	// Type defines a unique key.
-	// This help allow us to create the create when we only have the json representation of this data
-	Type events.EventObjectType `bson:"objectType" json:"objectType"`
-
-	App       events.App       `bson:"app" json:"app"`
-	Workspace events.Workspace `bson:"workspace" json:"workspace"`
-
-	Pricing OneTimePricing `bson:"pricing" json:"pricing"`
-
-	// BoughtAt defines when the app was purchased
-	BoughtAt time.Time `bson:"boughtAt" json:"boughtAt"`
-
-	// IsSelfManaged tells us whether the purchase is managed by the user or externally managed
-	IsSelfManaged bool `bson:"isSelfManaged" json:"isSelfManaged"`
-
-	// IsBundle tells us whether the purchase is a bundle
-	IsBundle bool `bson:"isBundle" json:"isBundle"`
-
-	// FromBundle defines if the app is inside a bundle and this event was triggered from a bundle
-	FromBundle bool `bson:"fromBundle" json:"fromBundle"`
-
-	// FromBundleID indicates which bundle id triggered this purchase event for the app
-	FromBundleID string `bson:"fromBundleId,omitempty" json:"fromBundleId,omitempty"`
+	Type          events.EventObjectType `bson:"objectType" json:"objectType"`
+	App           events.App             `bson:"app" json:"app"`
+	Workspace     events.Workspace       `bson:"workspace" json:"workspace"`
+	Pricing       OneTimePricing         `bson:"pricing" json:"pricing"`
+	BoughtAt      time.Time              `bson:"boughtAt" json:"boughtAt"`
+	IsSelfManaged bool                   `bson:"isSelfManaged" json:"isSelfManaged"`
+	IsBundle      bool                   `bson:"isBundle" json:"isBundle"`
+	FromBundle    bool                   `bson:"fromBundle" json:"fromBundle"`
+	FromBundleID  string                 `bson:"fromBundleId,omitempty" json:"fromBundleId,omitempty"`
 }
 
-// OneTimePricing defines the pricing and fields for a purchase
-// There is no way to have different tiers for an one time purchase
+// OneTimePricing defines the pricing and fields for a one-time purchase.
 type OneTimePricing struct {
 	Price float32 `bson:"price" json:"price"`
+}
+
+// NewPurchase creates a new Purchase event with the specified values.
+func NewPurchase(purchaseType events.EventObjectType, app events.App, workspace events.Workspace, pricing OneTimePricing, boughtAt time.Time, isSelfManaged, isBundle, fromBundle bool, fromBundleID string) Purchase {
+	return Purchase{
+		Type:          purchaseType,
+		App:           app,
+		Workspace:     workspace,
+		Pricing:       pricing,
+		BoughtAt:      boughtAt.UTC(),
+		IsSelfManaged: isSelfManaged,
+		IsBundle:      isBundle,
+		FromBundle:    fromBundle,
+		FromBundleID:  fromBundleID,
+	}
 }
